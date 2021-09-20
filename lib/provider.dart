9e17250ef965a1cd94b61a40c7f3fd30d921b827
mugri-ndeh <mugrindeh@gmail.com>
2021-09-20 23:59:18 +0100

    show dialog if win or draw

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -19,60 +20,102 @@ class TicTacGame with ChangeNotifier {
   List<int> clickedTiles = [];
   bool winner = false;
 
-  updateTile(int index) {
+  updateTile(int index, BuildContext context) {
     if (clickedTiles.isEmpty || !clickedTiles.contains(index)) {
       if (currentPlayer == player1) {
         clickedTiles.add(index);
         p1Tiles.add(index);
         tiles[index] = player1;
-        checkWinner('player1', p1Tiles);
+        checkWinner('player1', p1Tiles, context);
         currentPlayer = player2;
         notifyListeners();
       } else {
         clickedTiles.add(index);
         p2Tiles.add(index);
         tiles[index] = player2;
-        checkWinner('player2', p2Tiles);
+        checkWinner('player2', p2Tiles, context);
         currentPlayer = player1;
         notifyListeners();
       }
     }
   }
 
-  checkWinner(String player, List list) {
+  checkWinner(String player, List list, BuildContext context) {
     if (list.contains(0)) {
       if (list.contains(3) && list.contains(6)) {
         winner = true;
-        endgame(player, winner);
+        endgame(player, winner, context);
       } else if (list.contains(1) && list.contains(2)) {
         winner = true;
-        endgame(player, winner);
+        endgame(player, winner, context);
       } else if (list.contains(4) && list.contains(8)) {
         winner = true;
-        endgame(player, winner);
+        endgame(player, winner, context);
       }
     } else if (list.contains(6)) {
       if (list.contains(7) && list.contains(8)) {
         winner = true;
-        endgame(player, winner);
+        endgame(player, winner, context);
       } else if (list.contains(4) && list.contains(2)) {
         winner = true;
-        endgame(player, winner);
+        endgame(player, winner, context);
+      }
+    } else if (list.contains(4)) {
+      if (list.contains(1) && list.contains(7)) {
+        winner = true;
+        endgame(player, winner, context);
+      } else if (list.contains(3) && list.contains(5)) {
+        winner = true;
+        endgame(player, winner, context);
       }
     } else if (list.contains(2) && list.contains(5) && list.contains(8)) {
       winner = true;
-      endgame(player, winner);
+      endgame(player, winner, context);
     }
     if (clickedTiles.length == tiles.length && winner == false) {
       print('ended');
-      endgame(player, false);
+      endgame(player, false, context);
     }
   }
 
-  endgame(String player, bool winner) {
+  endgame(String player, bool winner, BuildContext context) {
     if (winner == true) {
       print('$player has won the game');
+      AwesomeDialog(
+        context: context,
+        animType: AnimType.SCALE,
+        dialogType: DialogType.SUCCES,
+        body: Center(
+          child: Text(
+            '$player has won the game',
+            style: TextStyle(fontStyle: FontStyle.normal),
+          ),
+        ),
+        title: 'This is Ignored',
+        desc: 'This is also Ignored',
+        btnOkOnPress: () {},
+      )..show();
+      Future.delayed(Duration(seconds: 1), () {
+        reset();
+      });
     } else {
+      AwesomeDialog(
+        context: context,
+        animType: AnimType.SCALE,
+        dialogType: DialogType.ERROR,
+        body: Center(
+          child: Text(
+            'Draw',
+            style: TextStyle(fontStyle: FontStyle.normal),
+          ),
+        ),
+        title: 'This is Ignored',
+        desc: 'This is also Ignored',
+        btnOkOnPress: () {},
+      )..show();
+      Future.delayed(Duration(seconds: 1), () {
+        reset();
+      });
       print('Draw');
     }
   }
