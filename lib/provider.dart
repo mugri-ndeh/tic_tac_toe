55aa9d6da43ce3a2cf6712803de5e70980641aea
mugri-ndeh <mugrindeh@gmail.com>
2021-09-25 02:38:29 +0100

    added comments

--- lib/provider.dart
+++ lib/provider.dart
@@ -4,33 +4,49 @@ import 'package:flutter/material.dart';
 
 class TicTacGame with ChangeNotifier {
   void init() {
+    //start with player 1
     currentPlayer = player1;
+    //initialize tile list to empty strings
     for (int i = 0; i < 9; i++) {
       tiles.add('');
     }
   }
 
+//player 1 and 2 symbols
   String player1 = 'X';
   String player2 = 'O';
-  String tapText = '';
+
+//used to keep track of tiles clicked by each player
   List p1Tiles = [];
   List p2Tiles = [];
+
+  //tiles on screen to be clicked
   List tiles = [];
   String currentPlayer = '';
+  //used to keep track of selected tiles
   List<int> clickedTiles = [];
+  //used to check if there was a winner or a draw
   bool winner = false;
 
   updateTile(int index, BuildContext context) {
+    //update tiles only if the board is empty or tile has not been selected
     if (clickedTiles.isEmpty || !clickedTiles.contains(index)) {
+      //player 1 plays
       if (currentPlayer == player1) {
+        //fill the clicked tile
         clickedTiles.add(index);
+        //tile index to user array to check  if use wins
         p1Tiles.add(index);
         tiles[index] = player1;
         checkWinner('player1', p1Tiles, context);
         currentPlayer = player2;
         notifyListeners();
-      } else {
+      }
+      //player 2 plays
+      else {
+        //fill the clicked tile
         clickedTiles.add(index);
+        //tile index to user array to check  if use wins
         p2Tiles.add(index);
         tiles[index] = player2;
         checkWinner('player2', p2Tiles, context);
@@ -40,47 +56,67 @@ class TicTacGame with ChangeNotifier {
     }
   }
 
+  //checking combinations
   checkWinner(String player, List list, BuildContext context) {
     if (list.contains(0)) {
+      //leftward column
       if (list.contains(3) && list.contains(6)) {
         winner = true;
         endgame(player, winner, context);
-      } else if (list.contains(1) && list.contains(2)) {
-        winner = true;
-        endgame(player, winner, context);
-      } else if (list.contains(4) && list.contains(8)) {
+      }
+      //top row
+      else if (list.contains(1) && list.contains(2)) {
         winner = true;
         endgame(player, winner, context);
       }
+      //diagonal
+      else if (list.contains(4) && list.contains(8)) {
+        winner = true;
+        endgame(player, winner, context);
+      }
+      //6 can  take leftward column and diagonal
     } else if (list.contains(6)) {
+      //bottom row
       if (list.contains(7) && list.contains(8)) {
         winner = true;
         endgame(player, winner, context);
-      } else if (list.contains(4) && list.contains(2)) {
+      }
+      //diagonal
+      else if (list.contains(4) && list.contains(2)) {
         winner = true;
         endgame(player, winner, context);
       }
-    } else if (list.contains(4)) {
+    } //taking 4 at the center of 3x3 grid
+    else if (list.contains(4)) {
+      //middle vertical
       if (list.contains(1) && list.contains(7)) {
         winner = true;
         endgame(player, winner, context);
-      } else if (list.contains(3) && list.contains(5)) {
+      }
+      //middle horizontal
+      else if (list.contains(3) && list.contains(5)) {
         winner = true;
         endgame(player, winner, context);
       }
-    } else if (list.contains(2) && list.contains(5) && list.contains(8)) {
+    }
+    //right most column
+    else if (list.contains(2) && list.contains(5) && list.contains(8)) {
       winner = true;
       endgame(player, winner, context);
     }
+    //if all tiles of board are selected with no winner
     if (clickedTiles.length == tiles.length && winner == false) {
       print('ended');
       endgame(player, false, context);
     }
   }
 
+//endgame in casee of draw or win
   endgame(String player, bool winner, BuildContext context) {
     if (winner == true) {
+      //debug purpose
       print('$player has won the game');
+      //show alert for a win
       AwesomeDialog(
         context: context,
         animType: AnimType.SCALE,
@@ -99,6 +135,7 @@ class TicTacGame with ChangeNotifier {
         reset();
       });
     } else {
+      //show alert for a draw
       AwesomeDialog(
         context: context,
         animType: AnimType.SCALE,
@@ -121,6 +158,7 @@ class TicTacGame with ChangeNotifier {
   }
 
   void reset() {
+    //clear the screen
     winner = false;
     p1Tiles = [];
     p2Tiles = [];
